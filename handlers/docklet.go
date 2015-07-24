6fc5369628915ef16c36928855bec64f0c82bc93
Keshav Varma <keshavdv@gmail.com>
2015-07-23 23:33:48 -0700

    Redo jobs

--- handlers/docklet.go
+++ handlers/docklet.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"os"
 
-	"bytes"
 	"github.com/coreos/go-etcd/etcd"
 	"github.com/fsouza/go-dockerclient"
 	"github.com/gorilla/context"
@@ -18,18 +17,23 @@ import (
 var docker_client *docker.Client
 var etcd_client *etcd.Client
 
-const JOB_PENDING = 0
-const JOB_SUCCESS = 1
-const JOB_FAILURE = 2
+const JOB_PENDING = "pending"
+const JOB_SUCCESS = "success"
+const JOB_FAILURE = "failed"
 
-var jobs map[string]int
+var jobs map[string]*Job
+
+type Job struct {
+	Status string            `json:"status"`
+	Data   map[string]string `json:"data,string"`
+}
 
 func init() {
 	var err error
 	c, _ := config.ReadDefault("./config/docklet.conf")
 	machines := []string{"http://localhost:4001"}
 	etcd_client = etcd.NewClient(machines)
-	jobs = make(map[string]int)
+	jobs = make(map[string]*Job)
 
 	docker_host, _ := c.String("docker", "host")
 	docker_port, _ := c.String("docker", "port")
@@ -44,25 +48,61 @@ func init() {
 	}
 }
 
+func createContainer(image string, cmd []string) (*docker.Container, error) {
+	container, err := docker_client.CreateContainer(
+		docker.CreateContainerOptions{
+			Config: &docker.Config{
+				OpenStdin:    true,
+				StdinOnce:    true,
+				AttachStdin:  true,
+				AttachStderr: true,
+				AttachStdout: true,
+				Image:        image,
+				Cmd:          cmd,
+				Tty:          true,
+			},
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	log.Println("created")
+
+	return container, nil
+}
+
 func pullImage(image string) (string, error) {
 	u, err := uuid.NewV4()
 	if err != nil {
 		return "", err
 	}
-	jobs[u.String()] = JOB_PENDING
+
 	go func() {
-		log.Println("TETS")
-		var buf bytes.Buffer
-		buf.Reset()
-		err := docker_client.PullImage(docker.PullImageOptions{Repository: image, OutputStream: &buf}, docker.AuthConfiguration{})
-		log.Println(buf.String())
+		jobs[u.String()] = &Job{JOB_PENDING, nil}
+		container, err := createContainer(image, []string{"/bin/bash"})
 		if err != nil {
-			log.Println(err.Error())
-			jobs[u.String()] = JOB_FAILURE
+			if err == docker.ErrNoSuchImage {
+				log.Println("I need to pull image")
+				err = docker_client.PullImage(docker.PullImageOptions{Repository: image}, docker.AuthConfiguration{})
+				if err != nil {
+					log.Println(err.Error())
+					jobs[u.String()] = &Job{JOB_FAILURE, map[string]string{"message": err.Error()}}
+				} else {
+					container, err = createContainer(image, []string{"/bin/bash"})
+					if err != nil {
+						jobs[u.String()] = &Job{JOB_FAILURE, map[string]string{"message": err.Error()}}
+					} else {
+						jobs[u.String()] = &Job{JOB_SUCCESS, map[string]string{"id": container.ID}}
+					}
+				}
+			} else {
+				log.Println("failure")
+				jobs[u.String()] = &Job{JOB_FAILURE, map[string]string{"message": err.Error()}}
+			}
 		} else {
-			jobs[u.String()] = JOB_SUCCESS
+			log.Println("hi")
+			jobs[u.String()] = &Job{JOB_SUCCESS, map[string]string{"id": container.ID}}
 		}
-		log.Println("DONE")
 	}()
 	return u.String(), nil
 }
@@ -76,21 +116,6 @@ func Build(w http.ResponseWriter, req *http.Request) {
 	// TODO: handle Dockerfile builds
 }
 
-func Pull(w http.ResponseWriter, req *http.Request) {
-	r := context.Get(req, "Render").(*render.Render)
-
-	image := req.URL.Query().Get("image")
-	if image == "" {
-		r.JSON(w, http.StatusBadRequest, map[string]string{"status": "invalid", "msg": "image must be specified"})
-		return
-	}
-
-	var buf bytes.Buffer
-	buf.Reset()
-	docker_client.PullImage(docker.PullImageOptions{Repository: image, OutputStream: &buf}, docker.AuthConfiguration{})
-	log.Println(buf.String())
-}
-
 func Create(w http.ResponseWriter, req *http.Request) {
 	r := context.Get(req, "Render").(*render.Render)
 
@@ -100,21 +125,12 @@ func Create(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	container, err := docker_client.CreateContainer(docker.CreateContainerOptions{Config: &docker.Config{OpenStdin: true, StdinOnce: true, AttachStdin: true, AttachStderr: true, AttachStdout: true, Image: image, Cmd: []string{"/bin/bash"}, Tty: true}})
+	job, err := pullImage(image)
 	if err != nil {
-		if err == docker.ErrNoSuchImage {
-			// attempt to pul
-			job, err := pullImage(image)
-			if err != nil {
-				r.JSON(w, http.StatusInternalServerError, map[string]string{"status": "failed"})
-			}
-			r.JSON(w, http.StatusOK, map[string]string{"status": "pending", "job": job})
-		} else {
-			r.JSON(w, http.StatusInternalServerError, map[string]string{"status": "failed", "msg": err.Error()})
-		}
-		return
+		r.JSON(w, http.StatusInternalServerError, map[string]string{"status": "failed"})
 	}
-	r.JSON(w, http.StatusOK, map[string]string{"status": "created", "id": container.ID})
+	r.JSON(w, http.StatusOK, map[string]string{"status": "pending", "job": job})
+
 }
 
 func Inspect(w http.ResponseWriter, req *http.Request) {
@@ -142,21 +158,10 @@ func Status(w http.ResponseWriter, req *http.Request) {
 		r.JSON(w, http.StatusBadRequest, map[string]string{"status": "invalid", "msg": "job id must be specified"})
 		return
 	}
-	if val, ok := jobs[id]; ok {
-		var status string
-
-		if val == JOB_SUCCESS {
-			status = "complete"
-		} else if val == JOB_FAILURE {
-			status = "failure"
-		} else {
-			status = "pending"
-		}
-
-		r.JSON(w, http.StatusOK, map[string]string{"status": status})
+	if job, ok := jobs[id]; ok {
+		r.JSON(w, http.StatusOK, job)
 	} else {
 		r.JSON(w, http.StatusNotFound, map[string]string{"status": "unknown"})
-
 	}
 }
 
